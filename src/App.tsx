803d68c6a2a49646c6dfcd2fa30a6ddaed737f20
Alcides Turruellas <ayunierto@gmail.com>
2024-05-08 07:41:42 -0500

    created button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
-import React from "react";
+import Button from "./packages/button/Button";
 
-type Props = {
-  /**
-   * Children to be rendered inside
-   */
-  children?: React.ReactNode;
-  /**
-   * Additional classes to be added
-   */
-  className?: string;
-  /**
-   * Additional styles to be added
-   */
-  style?: React.CSSProperties;
-};
-
-/**
- *
- */
-const App = ({ children, className = "", style }: Props) => {
+const App = () => {
   return (
-    <div className={`${className}`} style={style}>
-      {children}
-    </div>
+    <main>
+      <h1>Button</h1>
+      <p className="components__description">
+        Los botones permiten a los usuarios realizar acciones al hacer click
+      </p>
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Button variant="solid">deafult</Button>
+        </div>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <h2>Colors</h2>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Button color="default" variant="solid">
+            default
+          </Button>
+          <Button color="primary" variant="solid">
+            primary
+          </Button>
+          <Button color="success" variant="solid">
+            success
+          </Button>
+        </div>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <h2>Sizes</h2>
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "start",
+          }}
+        >
+          <Button size="small" color="default" variant="solid">
+            small
+          </Button>
+          <Button size="medium" color="default" variant="solid">
+            medium
+          </Button>
+          <Button size="large" color="default" variant="solid">
+            large
+          </Button>
+        </div>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <h2>Variants</h2>
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "start",
+          }}
+        >
+          <Button color="primary" variant="flat">
+            flat
+          </Button>
+          <Button color="primary" variant="solid">
+            solid
+          </Button>
+          <Button color="primary" variant="bordered">
+            bordered
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 };
 
---
+++ src/packages/button/Button.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+type Props = {
+  /**
+   * Children to be rendered inside the Button
+   */
+  children: React.ReactNode;
+  /**
+   * Additional classes to be added to the Button
+   */
+  className?: string;
+  /**
+   * Additional styles to be added to the Button
+   */
+  style?: React.CSSProperties;
+  /**
+   * Function to execute on click
+   */
+  onClick?: () => void;
+  /**
+   * Variant of the Button
+   */
+  variant?: "flat" | "solid" | "bordered";
+  /**
+   * Color of the Button
+   */
+  color?: "default" | "primary" | "success";
+  /**
+   * Size of the Button
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Disabled state of the Button
+   */
+  disabled?: boolean;
+  /**
+   * Type of the Button
+   */
+  type?: "button" | "submit" | "reset";
+  /**
+   * Add full width to the Button
+   */
+  block?: boolean;
+  /**
+   * Justify content to start
+   */
+  contentLeft?: boolean;
+};
+
+const Button = ({
+  children,
+  className = "",
+  style,
+  onClick,
+  color = "default",
+  variant = "solid",
+  size = "medium",
+  disabled,
+  type = "button",
+  block,
+  contentLeft,
+  ...props
+}: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+      className={`${styles[color]} ${styles[variant]} ${styles[size]} ${
+        disabled ? styles.disabled : ""
+      } ${block ? styles.block : ""} ${className} ${
+        contentLeft ? styles.content__left : ""
+      }`}
+      style={style}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
