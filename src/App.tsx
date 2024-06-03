ab1fef4bb26e016ef9c0900a7eef26bdd82dc5f5
Alcides Turruellas <ayunierto@gmail.com>
2024-06-03 06:34:46 -0500

    chadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 };
 
