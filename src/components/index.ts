2ec2ed7255a0325a7f2b680d9a5f631407f1f605
Alcides Turruellas <ayunierto@gmail.com>
2024-06-16 00:11:08 -0500

    adding typescript support

---
+++ src/components/index.ts
@@ -0,0 +1,42 @@
+export { Alert } from "./ui/alert";
+export { Button, buttonVariants } from "./ui/button";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+export { Flex } from "./ui/flex";
+export { Icon } from "./ui/icon/icon";
+export { Input } from "./ui/input";
